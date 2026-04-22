d9ba7e9818b82896e42c3bf81bd5f26f9439c34e
Francis Ametewee <fametewe@gmail.com>
2026-04-22 18:40:24 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int right = 1;
+        int profit = 0;
+
+        while (right < prices.size()) {
+            if (prices[left] < prices[right]) {
+                int newProfit = prices[right] - prices[left];
+                profit = std::max(profit, newProfit);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+        return profit;
+    }
+};

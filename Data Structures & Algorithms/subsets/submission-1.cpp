309d42afaa8d0057650650e06816f3cdbdf4f9ad
Francis Ametewee <fametewe@gmail.com>
2026-06-20 22:25:34 -0400

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void dfs(int index, vector<int>& subnet, vector<vector<int>>& res, const vector<int>& nums) {
+        if (index == nums.size()) {
+            res.push_back(subnet);
+            return;
+        }
+        dfs(index + 1, subnet, res, nums);
+        
+        subnet.push_back(nums[index]);
+        dfs(index + 1, subnet, res, nums);
+        subnet.pop_back();
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        std::vector<vector<int>> res;
+        std::vector<int> subnet;
+        dfs(0,  subnet, res, nums);
+        return res;
+    }
+};
